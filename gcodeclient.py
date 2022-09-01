63adaa117364a1398502c28dcfb417d297d0a579
Tony Jacob <tony2000jacob@gmail.com>
2022-09-01 14:58:18 +0530

    added absolute reading functionality

--- gcodeclient.py
+++ gcodeclient.py
@@ -15,37 +15,41 @@ class Client:
         self.ser = serial.Serial(port, baud)
         time.sleep(2)
 
-        self.command("G28")
-        # self.command("G28 X0 Y0 Z0\r\n")
-        # self.command("G28 X0 Y0\r\n")
-        # self.command("G28 X0\r\n")
-        # self.command("G28 Y0\r\n")
-        # self.command("G28 Z0\r\n")
+        #Keeping track of the position in absolute values
+        self.value_X = 0.0
+        self.value_Y = 0.0
+
+        self.__initialise("G28")
+        # self.__initialise("G28 X0 Y0 Z0\r\n")
+        # self.__initialise("G28 X0 Y0\r\n")
+        # self.__initialise("G28 X0\r\n")
+        # self.__initialise("G28 Y0\r\n")
+        # self.__initialise("G28 Z0\r\n")
 
         # Extruder Temp
-        # self.command("M104 S190 T0\r\n") #  start heating T0 to 190 degrees Celsius
-        # self.command("G28\r\n") # Home
-        # self.command("M109 S190 T0\r\n") # wait for T0 to reach 190 degrees before continuing with any other self.commands
+        # self.__initialise("M104 S190 T0\r\n") #  start heating T0 to 190 degrees Celsius
+        # self.__initialise("G28\r\n") # Home
+        # self.__initialise("M109 S190 T0\r\n") # wait for T0 to reach 190 degrees before continuing with any other self.commands
 
         # Bed Temp
-        # self.command("M140 S55\r\n") # heat bed to 50 degrees celsius but do not wait
-        # self.command("G28\r\n") # Home
-        # self.command("M190 S55\r\n") # wait for bed to heat to 50 degrees celsius and wait
+        # self.__initialise("M140 S55\r\n") # heat bed to 50 degrees celsius but do not wait
+        # self.__initialise("G28\r\n") # Home
+        # self.__initialise("M190 S55\r\n") # wait for bed to heat to 50 degrees celsius and wait
 
         # Fan
-        # self.command("M106 S255\r\n") # fan speed full
-        # self.command("M106 S127\r\n") # fan speed about half
-        # self.command("M106 S0\r\n") # turn off fan
+        # self.__initialise("M106 S255\r\n") # fan speed full
+        # self.__initialise("M106 S127\r\n") # fan speed about half
+        # self.__initialise("M106 S0\r\n") # turn off fan
 
         # Set Units(does not seem to work on ender 5)
-        # self.command("G20\r\n") # inches
-        self.command("G21") # millimeters
+        # self.__initialise("G20\r\n") # inches
+        self.__initialise("G21") # millimeters
 
         # Absolute Mode
-        # self.command("G90\r\n")
+        # self.__initialise("G90\r\n")
 
         # Relative Mode
-        self.command("G91\r\n")
+        self.__initialise("G91\r\n")
 
         # Move
         # self.command("G0 X7 Y18\r\n") # rapid motion but does not extrude material
@@ -58,20 +62,75 @@ class Client:
         Interfaces the Gcode commands to GRBL
         :param cmd:  A Gcode String.
         '''
-        try:   
+        try:
+            cmd = cmd.upper()
+            subcmds = cmd.split(" ")
+            for subcmd in subcmds:
+                if subcmd[0] == "X":
+                    self.value_X += float(subcmd[1:])
+                elif subcmd[0] == "Y":
+                    self.value_Y += float(subcmd[1:])
+            print(f'Value of X: {self.value_X}, y:{self.value_Y}')
             cmd = cmd + "\r\n"
             self.ser.write(str.encode(cmd))
             time.sleep(1)
             while True:
                 feedback = self.ser.readline()
                 if feedback == b'ok\r\n':
+                    # print(feedback)
                     break
         except TypeError:
             print("Gcode commands must be a string")
 
+
+    def __initialise(self, cmd):
+        '''
+        Same as that of command but for initialisation. Used in the constructor.
+        '''
+        cmd = cmd + "\r\n"
+        self.ser.write(str.encode(cmd))
+        time.sleep(1)
+        while True:
+            feedback = self.ser.readline()
+            if feedback == b'ok\r\n':
+                # print(feedback)
+                break
+
     def flush(self):
         '''
-        Use this function at the end of script. It closes the serial port.
+        Use this function to close the serial port.
         '''
         time.sleep(2)
         self.ser.close()
+
+    def manual_mode(self):
+        '''
+        Use this for sending one command at a time.
+        '''
+        while True:
+            string = input("Enter your Gcode / Enter q to quit / R to reset /H to origin: ")
+            string = string.upper()
+            print(string)
+            if string == "Q":
+                self.flush()
+                break
+
+            elif string == "R":
+                self.value_X = 0
+                self.value_Y = 0
+                print(f'Value of X: {self.value_X}, Y:{self.value_Y}')
+            
+            elif string == "H":
+                self.goto_origin()
+            
+            else:
+                self.command(string)
+
+    def goto_origin(self):
+        x = -self.value_X
+        y = -self.value_Y
+
+        x=str(x)
+        y=str(y)
+
+        self.command("G01 X"+x+" Y"+y)
